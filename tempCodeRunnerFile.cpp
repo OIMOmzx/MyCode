f4c41600e23a861b144d2b508fb5446023ab44d2
OIMOmzx <584084958@qq.com>
2022-08-03 22:20:17 +0800

    various

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,53 +1,58 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <cstdio>
+#include <unistd.h>
 using namespace std;
+int prime[1000010], k, n, cnt;
+bool vis[1000010];
 
-const int Max = 1000010;
-int t, n;
-string s;
-int trie[Max][27], res = 0;
-bool last[Max];
-int ans = 0;
-bool f = 0, flag = 0;
-
-bool insert(string s)
+long long quickpow(long long res, long long time, long long mod)
 {
-    int len = s.length(), p = 0;
-    for(int i = 0; i < len; i++)
-    {
-        int ch = s[i] - '0';
-        if(trie[p][ch] == 0)
-        {
-            trie[p][ch] = ++res;
-            f = 1;
-        }
-        p = trie[p][ch];
-        if(last[p]) flag = 1;
-    }
-    last[p] = 1;
-    return (flag == 1 || f == 0);
+	long long sum = 1;
+	while(time)
+	{
+		if(time % 2 == 1)
+		{
+			sum = sum * res % mod;
+		}
+		time /= 2;
+		res = res * res % mod;
+	}
+	return sum % mod;
 }
 
 int main()
 {
-    scanf("%d", &t);
-    while(t--)
+    k = 1000;
+    //cout << quickpow(3, 2019, 10) << endl;
+    vis[1] = 1;
+    for(int i = 2; i <= k; i++)
     {
-        memset(last, 0, sizeof(last));
-        memset(trie, 0, sizeof(trie));
-        bool ans = 0;
-        res = 0;
-        scanf("%d", &n);
-        for(int i = 1; i <= n; i++)
+        if(vis[i] == 0) prime[++cnt] = i;
+        for(int j = 1; j <= cnt && i * prime[j] <= k; j++)
         {
-            flag = f = 0;
-            cin >> s;
-            if(insert(s)) ans = 1;
+            vis[i * prime[j]] = 1;
+            if(i % prime[j] == 0) break;
         }
-        if(ans == 1) cout << "NO" << endl;
-        else cout << "YES" << endl;
     }
+    
+    for(int i = 2; i <= cnt; i++)
+    {
+        
+        for(int j = i + 1; j <= cnt; j++)
+        {
+            for(int k = 1; k <= 10000; k++)
+            {
+                int p = prime[i], q = prime[j];
+                cout << p << ", " << q <<  endl;
+                if((quickpow(k, q - 1, p) + q) % p == 0 && (quickpow(k, p - 1, q) + p) % q == 0)
+                {
+                    cout << "3ejwiucndniuenwrufoy8qc3w" << endl;
+                    cout << p << "," << q << "," << k;
+                    sleep(10);
+                }
+            }
+        }
+    }
+    
     return 0;
 }
