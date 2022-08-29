8d7cbcb4720327bb6cbfd83b0147652536ba4f48
OIMOmzx <584084958@qq.com>
2022-08-29 19:03:25 +0800

    2022.8.29upd6

---
+++ Codeforces/CodeForcesLast/Round812_Div2/A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int t, n, x[110], y[110];
+
+int main()
+{
+    cin >> t;
+    while(t--)
+    {
+        int a = 0, b = 0, c = 0, d = 0;
+        cin >> n;
+        for(int i = 1; i <= n; i++)
+        {
+            cin >> x[i] >> y[i];
+            if(x[i] < 0)
+            {
+                a = min(a, x[i]);
+            }
+            if(x[i] > 0)
+            {
+                b = max(b, x[i]);
+            }
+            if(y[i] < 0)
+            {
+                c = min(c, y[i]);
+            }
+            if(y[i] > 0)
+            {
+                d = max(d, y[i]);
+            }
+        }
+        //cout << a << ", " << b << ", " << c << ", " << d << endl;
+        cout << (abs(a) + abs(b) + abs(c) + abs(d)) * 2 << endl;
+    }
+    return 0;
+}
