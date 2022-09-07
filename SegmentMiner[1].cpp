03cf9669e9d510f37a9f279bea90fd5e4293c05b
OIMOmzx <584084958@qq.com>
2022-09-07 20:02:27 +0800

    MMO3

--- SegmentMiner[1].cpp
+++ SegmentMiner[1].cpp
@@ -16,7 +16,7 @@ int head[Max * 10];
 int value_of_all_node[Max * 10];
 int if_bomb_of_all_node[Max * 10];
 char name_of_all_node[Max * 10];//每个节点的名字，用桶数组形式存储
-bool if_of_all_node[Max * 10];//这个编号的点，是否是特殊点
+bool if_special_of_all_node[Max * 10];//这个编号的点，是否是特殊点
 
 struct node
 {
@@ -33,9 +33,17 @@ void add(int u, int v, int num_of_node_on_the_side)
     head[u] = num_of_side++;
 }//链式前向星
 
-int main()
+void init()
 {
     memset(head, -1, sizeof(head));
+    for(int i = 0; i <= Max * 10 - 1; i++)
+    {
+        if(i % 10 == 0) if_bomb_of_all_node[i] = 1;
+    }
+}
+
+int main()
+{
     cout << "请输入一共有多少个点" << endl;
     cin >> num_of_node;
     cout << "请输入特殊点的个数" << endl;
@@ -47,6 +55,24 @@ int main()
         cout << "然后输入节点的编号";
         cin >> name >> num;
         name_of_all_node[num] = name;
+        if_special_of_all_node[num] = name;
+    }
+    while(1)//由许多轮组成的本场游戏
+    {
+        //#TODO:待补充结束条件
+        while(1)//不限参与人数
+        {
+            //#TODO:玩家身份认证
+            int choose;
+            cout << "选择哪一个节点" << endl;
+            if(if_bomb_of_all_node[choose] == 1)
+            {
+                //#TODO:开局死亡提醒；
+                //cout << 
+            }
+            //#TODO:需要记录，走了几步，本轮得到矿的总量，得到矿的总量，maybe目前总排名，maybe当前局排名
+            //#TODO:走到特殊点行走处理，including bfs指令, [后期]tp, 探测器，灌水等功能开发
+        }
     }
     return 0;
 }
